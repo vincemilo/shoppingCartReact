217cf2b501fead6f6afc9d316ccede557c0f9ee1
Vince Milo <1554999+vincemilo@users.noreply.github.com>
2024-07-06 18:42:03 -0600

    finally got getFetch mock to work! thanks to bender at top

--- src/components/product/Product.test.jsx
+++ src/components/product/Product.test.jsx
@@ -1,14 +1,24 @@
 import Product from "./Product";
 import { render, screen } from "@testing-library/react";
 import { test, vi, expect } from "vitest";
+import useFetch from "../../useFetch";
 
 vi.mock("react-router-dom", () => ({
   useOutletContext: () => [vi.fn()],
   useParams: () => ({ id: "1" }),
 }));
 
-function mockUseFetch() {
-  return {
+vi.mock("../../useFetch");
+
+test("shows loading text while API request is in progress", () => {
+  useFetch.mockReturnValue({ loading: true });
+  render(<Product />);
+  const loading = screen.getByText("Loading...");
+  expect(loading).toBeInTheDocument();
+});
+
+test("renders product's name", () => {
+  useFetch.mockReturnValue({
     data: {
       id: 1,
       title: "Test Product",
@@ -17,26 +27,15 @@ function mockUseFetch() {
       rating: { rate: 4.5 },
       description: "This is a test product description.",
     },
-    loading: false,
-    error: null,
-  };
-}
-
-vi.mock("../../useFetch", () => {
-  return {
-    default: mockUseFetch,
-  };
-});
-
-test("loading text is shown while API request is in progress", async () => {
+  });
   render(<Product />);
-  const loading = screen.getByText("Loading...");
-
-  expect(loading).toBeInTheDocument();
-});
-
-test("product's name is rendered", async () => {
-  render(<Product />);
-  const productName = await screen.findByText("Test Product");
+  const productName = screen.getByText("Test Product");
   expect(productName).toBeInTheDocument();
+});
+
+test("shows error message when there is an error", () => {
+  useFetch.mockReturnValue({ error: new Error() });
+  render(<Product />);
+  const errorMessage = screen.getByText("A network error was encountered");
+  expect(errorMessage).toBeInTheDocument();
 });
